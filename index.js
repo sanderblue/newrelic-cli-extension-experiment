bfefef69862462c56c99280a8ec562dca7b2b497
Sander Blue <sblue@newrelic.com>
2020-05-06 17:18:34 -0700

    wip: committing updates

--- index.js
+++ index.js
@@ -10,47 +10,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const http = require("http");
+const axios_1 = require("axios");
+const client = axios_1.default.create({
+    baseURL: 'http://localhost:8080',
+});
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        http
-            .get('http://localhost:8080', (res) => {
-            const { statusCode } = res;
-            const contentType = res.headers['content-type'];
-            let error;
-            if (statusCode !== 200) {
-                error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+        console.log('Node - making HTTP request');
+        const { data } = yield client.get('/command');
+        console.log('Node - cmdResp:', JSON.stringify(data, null, 2));
+        if (data.interactive) {
+            for (const f of data.flags) {
+                const promptResp = yield client.post('/prompt', {
+                    options: f.options,
+                    prompt: f.prompt,
+                });
+                console.log('Node - promptResp', promptResp.data);
             }
-            else if (!/^application\/json/.test(contentType)) {
-                error = new Error('Invalid content-type.\n' +
-                    `Expected application/json but received ${contentType}`);
-            }
-            if (error) {
-                console.error(error.message);
-                // Consume response data to free up memory
-                res.resume();
-                return;
-            }
-            res.setEncoding('utf8');
-            let rawData = '';
-            res.on('data', (chunk) => {
-                console.log('res.on - data:', chunk);
-                rawData += chunk;
-            });
-            res.on('end', (a) => {
-                console.log('res.on - end: ', a);
-                try {
-                    const parsedData = JSON.parse(rawData);
-                    console.log('res parsed data:', parsedData);
-                }
-                catch (e) {
-                    console.error(e.message);
-                }
-            });
-        })
-            .on('error', (e) => {
-            console.error(`Got error: ${e.message}`);
-        });
+        }
     });
 }
 try {
@@ -59,19 +36,3 @@ try {
 catch (error) {
     console.log('ERROR:', error);
 }
-// const client = axios.create({
-//   baseURL: 'http://localhost:8080',
-// });
-// async function run(): Promise<void> {
-//   console.log('Node - making HTTP request');
-//   const resp = await client.get<CLIResponse>('');
-//   // resp.data will be typed based on the
-//   // provided CLIResponse interface
-//   console.log("");
-//   console.log('Node - resp:', resp.data);
-// }
-// try {
-//   run();
-// } catch (error) {
-//   console.log('ERROR:', error);
-// }
--- index.ts
+++ index.ts
@@ -3,60 +3,41 @@
 import * as http from 'http';
 import axios from 'axios';
 
-interface CLIResponse {
-  Cmd: string;
-  Args: string[];
-  Flags: {
-    [key: string]: string;
-  };
+interface Flag {
+  name: string;
+  value: string;
+  options: string[];
+  prompt: string;
 }
 
-async function run() {
-  http
-    .get('http://localhost:8080', (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type'];
+interface CLIResponse {
+  cmd: string;
+  args: string[];
+  flags: Flag[];
+  interactive: boolean;
+}
 
-      let error: Error;
-      if (statusCode !== 200) {
-        error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error(
-          'Invalid content-type.\n' +
-            `Expected application/json but received ${contentType}`,
-        );
-      }
+const client = axios.create({
+  baseURL: 'http://localhost:8080',
+});
 
-      if (error) {
-        console.error(error.message);
-        // Consume response data to free up memory
-        res.resume();
-        return;
-      }
+async function run(): Promise<void> {
+  console.log('Node - making HTTP request');
 
-      res.setEncoding('utf8');
+  const { data } = await client.get<CLIResponse>('/command');
 
-      let rawData = '';
-      res.on('data', (chunk) => {
-        console.log('res.on - data:', chunk);
+  console.log('Node - cmdResp:', JSON.stringify(data, null, 2));
 
-        rawData += chunk;
+  if (data.interactive) {
+    for (const f of data.flags) {
+      const promptResp = await client.post('/prompt', {
+        options: f.options,
+        prompt: f.prompt,
       });
 
-      res.on('end', (a) => {
-        console.log('res.on - end: ', a);
-
-        try {
-          const parsedData = JSON.parse(rawData);
-          console.log('res parsed data:', parsedData);
-        } catch (e) {
-          console.error(e.message);
-        }
-      });
-    })
-    .on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-    });
+      console.log('Node - promptResp', promptResp.data);
+    }
+  }
 }
 
 try {
@@ -64,24 +45,3 @@ try {
 } catch (error) {
   console.log('ERROR:', error);
 }
-
-// const client = axios.create({
-//   baseURL: 'http://localhost:8080',
-// });
-
-// async function run(): Promise<void> {
-//   console.log('Node - making HTTP request');
-
-//   const resp = await client.get<CLIResponse>('');
-
-//   // resp.data will be typed based on the
-//   // provided CLIResponse interface
-//   console.log("");
-//   console.log('Node - resp:', resp.data);
-// }
-
-// try {
-//   run();
-// } catch (error) {
-//   console.log('ERROR:', error);
-// }
